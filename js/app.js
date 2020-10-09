9bb770a6e76ac0c8edf2192ce004e3c1160f4bbc
Robert_Norwood <robnorwood91@gmail.com>
2020-10-09 08:12:19 -0700

    updating this

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const wordBank =
 let secretWord, maxGuesses, guessesMade, keyboardButtons, countdown, correctLetters
 secretWord = ''
 maxGuesses = 6
-guessesMade = 0
+guessesMade = 5
 keyboardButtons = 
 ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
