e3d760d33fe9b3fb0e61d2cf38dc84a1605a55d6
Robert_Norwood <robnorwood91@gmail.com>
2020-10-14 12:19:07 -0700

    updating the game

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-// generate a word bank to pick random word from 
+//*******************************************************************************************************
+
 const wordBank = 
 ['dog', 'cat', 'lion', 'bear', 'wolf', 'rabbit', 'dolphin',
  'shark', 'tiger', 'penguin', 'rhino', 'hedgehog', 'hamster',
@@ -6,213 +7,228 @@ const wordBank =
  'gorilla', 'wolverine', 'woodpecker', 'mammoth', 'elephant',
  'crocodile', 'dragonfly', 'gecko']
 
-let secretWord, maxGuesses, guessesMade, keyboardButtons, countdown, correctLetters
+//*******************************************************************************************************
+
+let secretWord, maxGuesses, guessesMade, userGuessedArray, userWord, intervalTimer, clockCounter
 secretWord = ''
 maxGuesses = 6
-guessesMade = 5
-keyboardButtons = 
-['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
-'x', 'y', 'z']
-countdown = 0
-correctLetters = []
+guessesMade = 0
+userGuessedArray = []
+userWord = null
+intervalTimer = null
+clockCounter = 30
 
+//*******************************************************************************************************
 
-// generate random word
+let hangmanImg, underscores, keyboard, userWrongGuesses, runningClock, playerHeaders, resetButton, startButton, instructions
+
+hangmanImg = document.getElementById('hangman')
+underscores = document.getElementById('underscores')
+keyboard = document.getElementById('keyboard')
+userWrongGuesses = document.getElementById('guesses')
+runningClock = document.getElementById('time')
+playerHeaders = document.getElementById('playerHeadings')
+resetButton = document.getElementById('resetGame')
+startButton = document.getElementById('startGame')
+instructions = document.getElementById('instructionParas')
+
+//*******************************************************************************************************
+const startThirtySecondTimer = () => {
+	intervalTimer = setInterval(thirtySecondTimer, 1000)
+
+}
+
+const stopCountdown = () => {
+	clearInterval(intervalTimer)
+}
+
+const thirtySecondTimer = () => {
+	clockCounter--
+	if(clockCounter > 0) {
+		runningClock.innerHTML = clockCounter
+	} else if (clockCounter === 0) {
+		underscores.innerHTML = 'TIME RAN OUT, YOU LOSE'
+		hideKeyBoard()
+		hideTimer()
+	}	
+}
+
+//*******************************************************************************************************
+
 const generateRandomWord = () => {
-	secretWord = wordBank[Math.floor(Math.random() * wordBank.length)]
-	console.log(secretWord)
-}
-generateRandomWord()
-
-// generate a way for the user to pick a letter - DONE
-// set an event listener on the button so when clicked, you get the letter on the button - DONE
-const generateKeyboard = () => {
-	for (let i = 0; i < keyboardButtons.length; i++) {
-		//create a button every loop
-		let button = document.createElement('BUTTON')
-		//set the text as the current element
-		button.innerHTML = keyboardButtons[i]
-		//give a class of the below to each button
-		button.className = 'btn m-1'
-		//give each button an id of the current element (so letter 'a' will have an id of 'a')
-		button.setAttribute('id', keyboardButtons[i])
-		// change background of each button to olive
-		button.style.background = 'olive'
-		// give each button a border of the below
-		button.style.border = '1px solid black'
-		// append each button to the element with the id of buttons
-		document.getElementById('keyboard').appendChild(button)
-		// on click, return the letter on the button
-		button.addEventListener('click', () => {
-			return keyboardButtons[i] 
-		})
-	}
+  secretWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+  console.log(secretWord)
 }
 
-// generate a countdown timer - 30 seconds - DONE
-	//when the timer gets to 0 turn is over
-	let runningTimer = () => {
-		countdown = 30
-		// use the setInterval method
-		setInterval(function() {
-			// decrement the countdown variable
-			countdown--
-			// if countdown is greater than or equal to 0
-			if (countdown > 0) {
-				// set counterId equal to the element with an id of 'time', in this case this its a span tag with the id 'time'
-				counterId = document.getElementById('time')
-				// set the html of counterId to equal the above variable, countdown
-				counterId.innerHTML = countdown
-			} else if (countdown === 0) {
-				document.getElementById('underscores').innerHTML = 'TIME RAN OUT, YOU LOSE!'
-		}
-	}, 1000)
+//*******************************************************************************************************
+
+const generateKeyboardButtons = () => {
+
+ 	let keyboardButtons = 
+  	'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+  	`<button class="btn btn-lg m-1 keyboardButtons" id='` + letter + `'onClick="handleUserGuess
+  	('` + letter + `')" style="background: olive">` + letter + `</button>`).join('');
+
+	keyboard.innerHTML = keyboardButtons;
 }
 
-// generateUnderscores to replace word with underscores on user interface
-let generateUnderscores = () => {
-	for(let i = 0; i < secretWord.length; i++) {
-		correctLetters[i] = "_"
-	}
-	document.getElementById('underscores').innerHTML = correctLetters.join(" ")
+//*******************************************************************************************************
+
+const handleUserGuess = (userLetter) => {
+	//if the letter the user picked isn't in the userGuessedArray
+	// then push the letter to the array OR if it is present, then do nothing
+  userGuessedArray.includes(userLetter) !== true ? userGuessedArray.push(userLetter) : null;
+  // disable the button (letter) that the user picks after either pushing to the 
+  // UGArray or after doing nothing, so they know not to use that letter again
+  document.getElementById(userLetter).setAttribute('disabled', true);
+  // if the letter the user picked is present inside of the secret word
+  if (secretWord.includes(userLetter) === true) {
+    userGuessedWord();
+    checkIfUserWonGame();
+    // else if the letter the user picked isn't in the secret word
+  } else if (secretWord.includes(userLetter) !== true) {
+    guessesMade++;
+    userGuesses()
+    checkIfUserLostGame();
+    updateHangmanPicture();
+  }
 }
 
-generateUnderscores()
+//*******************************************************************************************************
 
-// update the hangman image 
-const updateHangingImg = () => {
-	document.getElementById('hangman').src = './images/' + guessesMade + '.jpg'
+const updateHangmanPicture = () => {
+  hangmanImg.src = './images/' + guessesMade + '.jpg';
 }
 
-//show guesses on page 
+//*******************************************************************************************************
+
+const checkIfUserWonGame = () => {
+  if (userWord === secretWord) {
+    underscores.innerHTML = "YOUR GUESS WAS CORRECT YOU WON THE GAME!";
+    hideKeyBoard()
+    stopCountdown()
+  }
+}
+
+//*******************************************************************************************************
+
+
+const checkIfUserLostGame = () => {
+  if (guessesMade === maxGuesses) {
+    underscores.innerHTML = "YOU RAN OUT OF GUESSES YOU LOSE!";
+    hideKeyBoard()
+    stopCountdown()
+  }
+}
+
+//*******************************************************************************************************
+
+const userGuessedWord = () => {
+	// split the secret word into an array, iterate over the elements in the array using map, then check to see if the letter the user chose is present inside of the secret word, if it is then show the letter, if it isn't then show the underscore
+  userWord = secretWord.split('').map(letter => (userGuessedArray.includes(letter) === true ? letter : " _ ")).join('');
+
+  underscores.innerHTML = userWord;
+}
+
+//*******************************************************************************************************
+
+const userGuesses = () => {
+	userWrongGuesses.innerHTML = "Guesses Made: " + guessesMade + " of " + maxGuesses + "!"
+}
+
+//*******************************************************************************************************
+
+
+const hidePlayerHeadings = () => {
+	playerHeaders.style.display = 'none'
+}
+
+const showPlayerHeadings = () => {
+	playerHeaders.style.display = 'flex'
+}
+
+const hideKeyBoard = () => {
+	keyboard.style.display = 'none'
+}
+const showKeyBoard = () => {
+	keyboard.style.display = 'block'
+}
+const hideHangmanPic = () => {
+	hangmanImg.style.display = 'none'
+}
+
+const showHangmanPic = () => {
+	hangmanImg.style.display = 'block'
+}
+
+const hideUserGuesses = () => {
+	userWrongGuesses.style.display = 'none'
+}
+
 const showUserGuesses = () => {
-	let showGuesses = document.getElementById('guesses')
-	showGuesses.innerHTML = "Guesses Made: " + guessesMade + " of " + maxGuesses + "!"
+	userWrongGuesses.style.display = 'block'
 }
 
-
-// check if user lost game
-const userLostGame = () => {
-	if (guessesMade === maxGuesses){
-		document.getElementById('underscores').innerHTML = "YOU'VE RUN OUT OF GUESSES, YOU LOSE!"
-	}
+const hideTimer = () => {
+	runningClock.style.display = 'none'
 }
 
-// reset the game 
-const startNewGame = () => {
-	guessesMade = 0
-	document.getElementById('hangman').src = './images/0.jpg'
-	generateRandomWord()
+const showTimer = () => {
+	runningClock.style.display = 'flex'
+}
+
+const hideResetButton = () => {
+	resetButton.style.display = 'none'
+}
+
+const showResetButton = () => {
+	resetButton.style.display = 'block'
+}
+
+const hideInstructions = () => {
+	instructions.style.display = 'none'
+}
+
+const showInstructions = () => {
+	instructions.style.display = 'block'
+}
+
+//*******************************************************************************************************
+
+const unhideGame = () => {
+	hideInstructions()
+	showPlayerHeadings()
+	showTimer()
+	showHangmanPic()
 	showUserGuesses()
-	generateKeyboard()
+	showResetButton()
+	startGame()
 }
 
-generateRandomWord()
-generateKeyboard()
-runningTimer()
-generateUnderscores()
-updateHangingImg()
-showUserGuesses()
-userLostGame()
+//*******************************************************************************************************
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Check to see if the letter the user picks is in the random word
-	// if the letter is in the random word, do something
-	// if the letter isn't in the random word, do something else
-
-// keep track of the letters the player guessed
-	// push to a lettersguessed array
-
-// check to see if the user guessed the correct word
-	// if so, do something
-	//update player score
-	// reset timer
-	// generate new word
-
-// check to see if the user lost the game or ran out of allowed mistakes
-	// if so, do something
-	// reset the game
-	// generate new word
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-playerOneScore = 0
-playerTwoScore = 0
-let updatePlayer1 = document.getElementById('score1')
-let updatePlayer2 = document.getElementById('score2')
-
-
-const getUserGuess = (letterPicked) => {
-	guessedLetter.indexOf(letterPicked) === -1 ? guessedLetter.push(letterPicked) : null
+const startGame = () => {
+	startThirtySecondTimer()
+	generateRandomWord()
+	generateKeyboardButtons()
+	userGuessedWord()
+	userGuesses()
 }
 
+//*******************************************************************************************************
 
-// generate underscores to show how many letters are in the word
-
-
-const generateUnderscores = () => {
-	// split the random word into an array
-	answerArray = secretWord.split('')
-	// check to see if any of the letters (elements) in the answerArray are in the guessed letters array above. 
-	// join either the element or underscore on an empty string to get rid of the commas
-	userLetter = answerArray.map(element => (guessedLetter.indexOf(element) >= 0 ? element : " _ ")).join("")
-		document.getElementById('underscores').innerHTML = userLetter
-	
+const resetGame = () => {	
+	guessesMade = 0
+	userGuessedArray = []
+	hangmanImg.src = './images/0.jpg';
+	clockCounter = 30
+	startGame()
+	showKeyBoard()
 }
-generateUnderscores()
 
+showInstructions()
+hidePlayerHeadings()
+hideResetButton()
+hideHangmanPic()
+hideUserGuesses()
 
-const checkGuess = (letterSelected) => {
-	if (answer.includes(letterSelected) === true) {
-
-	} else if (answer.includes(letterSelected) === false) {
-
-	}
-
-	
-	
-}
-		
-*/
